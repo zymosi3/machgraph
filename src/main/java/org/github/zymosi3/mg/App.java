ac4ac72a3e4806f97c73e923f7acd6050d2c510c
Anton Tarasenko <zymosi3@gmail.com>
2017-03-08 12:52:38 +0600

    bresenham and float triangle

--- src/main/java/org/github/zymosi3/mg/App.java
+++ src/main/java/org/github/zymosi3/mg/App.java
@@ -15,6 +15,8 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 
@@ -23,7 +25,7 @@ public class App {
     private static final int WIDTH = 1024;
     private static final int HEIGHT = 768;
 
-    private static final float SCALE = 1f;
+    private static final float SCALE = .1f;
 
     private static final int SWIDTH = (int) (WIDTH * SCALE);
     private static final int SHEIGHT = (int) (HEIGHT * SCALE);
@@ -87,7 +89,7 @@ public class App {
             }
         }, "render").start();
 
-//        new Thread(() -> draw(drawer), "draw").start();
+//        new Thread(() -> draw(drawer, objects), "draw").start();
     }
 
     private static List<Obj> loadObjects() {
@@ -100,7 +102,12 @@ public class App {
                 WavefrontStream.ofResource("cube").get(),
                 new Motion(new Vec3(0.5f, 0.5f, 0.5f), new Vec3(0.0f, 0.0f, 0.0f))
         );
-        return Collections.singletonList(cube);
+        Obj triangle = new Obj(
+                new Position(A, A1, X0),
+                WavefrontStream.ofResource("triangle").get(),
+                new Motion(new Vec3(0.5f, 0.5f, 0.5f), new Vec3(0.0f, 0.0f, 0.0f))
+        );
+        return Collections.singletonList(triangle);
     }
 
     private static void draw(Drawer drawer, List<Obj> objects) {
@@ -109,23 +116,63 @@ public class App {
 //        FileStreamer.ofResource("debug-bresenham").get().forEach(c -> c.accept(drawer));
 //        FileStreamer.ofResource("4dim-cube").get().forEach(c -> c.accept(drawer));
 
+        int color = Drawer.color(255, 255, 255);
+        drawer.triangleBresenham(10, 10, 15, 20, 20, 10, color);
+        drawer.line(35, 20, 30, 10, color);
+        drawer.line(35, 20, 40, 10, color);
+        drawer.line(30, 10, 40, 10, color);
+
+        drawer.triangleBresenham(50, 20, 60, 20, 55, 10, color);
+        drawer.line(75, 10, 70, 20, color);
+        drawer.line(75, 10, 80, 20, color);
+        drawer.line(70, 20, 80, 20, color);
+
+        drawer.triangleBresenham(10, 25, 20, 45, 25, 30, color);
+        drawer.line(40, 25, 50, 45, color);
+        drawer.line(50, 45, 55, 30, color);
+        drawer.line(55, 30, 40, 25, color);
+
+        drawer.triangleBresenham(10, 50, 20, 70, 18, 55, color);
+        drawer.line(40, 50, 50, 70, color);
+        drawer.line(50, 70, 48, 55, color);
+        drawer.line(48, 55, 40, 50, color);
+
 
         Function<Vec3, Vec3> projection = new PerspectiveProjection(0.83f);
         Function<Vec3, Vec3> screenScale = new ScreenScale(WIDTH, HEIGHT);
-
+//
         BinaryOperator<Vec3> drawLine = new DrawLine(drawer);
 
-        objects.stream().
-                map(obj -> obj.move.apply(obj)).
-                forEach(obj -> obj.stream().forEach(face ->
-                        face.stream().
-                                map(obj.obj2Screen).
-                                map(projection).
-                                map(screenScale).
-                                reduce(drawLine)
-                ));
+//        objects.stream().
+//                map(obj -> obj.move.apply(obj)).
+//                forEach(obj -> obj.stream().forEach(face ->
+//                        {
+//                            Vec3[] vertices = face.stream().
+//                                    map(obj.obj2Screen).
+//                                    map(projection).
+//                                    map(screenScale).
+//                                    toArray(Vec3[]::new);
+//                            drawer.triangleBresenham(
+//                                    Math.round(vertices[0].x), Math.round(vertices[0].y),
+//                                    Math.round(vertices[1].x), Math.round(vertices[1].y),
+//                                    Math.round(vertices[2].x), Math.round(vertices[2].y),
+//                                    Drawer.color(255, 255, 255)
+//                            );
+//                        }
+//                ));
+
+//        objects.stream().
+//                map(obj -> obj.move.apply(obj)).
+//                forEach(obj -> obj.stream().forEach(face ->
+//                        face.stream().
+//                                map(obj.obj2Screen).
+//                                map(projection).
+//                                map(screenScale).
+//                                reduce(drawLine)
+//                ));
 
 //        AtomicInteger i = new AtomicInteger();
+//        Random r = new Random();
 //        WavefrontStream.ofResource("african_head").get().
 //                peek(f -> {
 //                    if (Global.DEBUG)
@@ -136,11 +183,19 @@ public class App {
 ////                        e.printStackTrace();
 ////                    }
 //                }).
-//                forEach(face ->
-//                        face.stream().
-//                                map(v -> new Vec3(v.x, v.y + 0.5f, v.z)).
-//                                map(screenScale).
-//                                reduce(line)
+//                forEach(face -> {
+//                            Vec3[] vertices =
+//                                    face.stream().
+//                                            map(v -> new Vec3(v.x, v.y + 0.5f, v.z)).
+//                                            map(screenScale).
+//                                            toArray(Vec3[]::new);
+//                                    drawer.triangle(
+//                                    Math.round(vertices[0].x), Math.round(vertices[0].y),
+//                                    Math.round(vertices[1].x), Math.round(vertices[1].y),
+//                                    Math.round(vertices[2].x), Math.round(vertices[2].y),
+//                                    Drawer.color(r.nextInt(255), r.nextInt(255), r.nextInt(255))
+//                            );
+//                        }
 //                );
 
         if (Global.DEBUG)
--- src/main/java/org/github/zymosi3/mg/Drawer.java
+++ src/main/java/org/github/zymosi3/mg/Drawer.java
@@ -1,7 +1,9 @@
 package org.github.zymosi3.mg;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
+@SuppressWarnings("Duplicates")
 public class Drawer {
 
     private final int[] pixels;
@@ -51,7 +53,7 @@ public class Drawer {
         for (int i = 0; i <= dx; i++) {
             point(x, y, color);
             // By Rodgers here should be
-            // while (error >= 0) {
+//             while (error >= 0) {
             // this modification done to make lanes, drawn from right to left and from left to right, match
             // s2 >= 0 means that we in 1st or 2nd octant
             // s2 < 0 means that we in 3rd or 4th octant
@@ -68,6 +70,185 @@ public class Drawer {
                 x += s1;
             error += 2 * dy;
         }
+    }
+
+    public void triangleBresenham(int x0, int y0, int x1, int y1, int x2, int y2, int color) {
+        int[][] vertices = new int[][] { new int[] { x0, y0 }, new int[] { x1, y1 }, new int[] { x2, y2 } };
+        Arrays.sort(vertices, Comparator.comparingInt(v -> v[1]));
+        triangleBresenhamSorted(
+                vertices[0][0],
+                vertices[0][1],
+                vertices[1][0],
+                vertices[1][1],
+                vertices[2][0],
+                vertices[2][1],
+                color
+        );
+    }
+
+    private void triangleBresenhamSorted(int x0, int y0, int x1, int y1, int x2, int y2, int color) {
+        // here we know that y0 <= y2 <= y3
+        if (y1 == y2) { // check for trivial case of bottom-flat triangle
+            fillOneSideFlatTriangle(x1, y1, x2, y2, x0, y0, color);
+        } else if (y0 == y1) { // check for trivial case of top-flat triangle
+            fillOneSideFlatTriangle(x0, y0, x1, y1, x2, y2, color);
+        } else {
+            // general case - split the triangle in a topflat and bottom-flat one
+            int x3 = Math.round(x0 + ((float)(y1 - y0) / (float)(y2 - y0)) * (x2 - x0));
+            fillOneSideFlatTriangle(x1, y1, x3, y1, x0, y0, color);
+            fillOneSideFlatTriangle(x1, y1, x3, y1, x2, y2, color);
+        }
+    }
+
+    private void fillOneSideFlatTriangle(int x0, int y0, int x1, int y1, int x2, int y2, int color) {
+        int x_0 = x2; // x for the one side line
+        int x_1 = x2; // x for the other side line
+        int y_0 = y2;
+        int y_1 = y2;
+        int dx_0 = Math.abs(x0 - x2);
+        int dx_1 = Math.abs(x1 - x2);
+        int dy_0 = Math.abs(y0 - y2);
+        int dy_1 = Math.abs(y1 - y2);
+        int sx_0 = Integer.signum(x0 - x2);
+        int sx_1 = Integer.signum(x1 - x2);
+        int sy_0 = Integer.signum(y0 - y2);
+        int sy_1 = Integer.signum(y1 - y2);
+
+        boolean swap_0 = false;
+        if (dy_0 > dx_0) {
+            int v = dx_0;
+            dx_0 = dy_0;
+            dy_0 = v;
+            swap_0 = true;
+        }
+
+        boolean swap_1 = false;
+
+        if (dy_1 > dx_1) {
+            int v = dx_1;
+            dx_1 = dy_1;
+            dy_1 = v;
+            swap_1 = true;
+        }
+
+        int error_0 = 2 * dy_0 - dx_0;
+        int error_1 = 2 * dy_1 - dx_1;
+
+        boolean stop_0 = false;
+        boolean stop_1 = false;
+        for (int i_0 = 0, i_1 = 0; i_0 <= dx_0 || i_1 <= dx_1; ) {
+            if (stop_0 && stop_1) {
+                line(x_0, y_0, x_1, y_1, color);
+                stop_0 = false;
+                stop_1 = false;
+            }
+            if (i_0 <= dx_0 && !stop_0) {
+                point(x_0, y_0, color);
+                while ((sy_0 > 0 && error_0 >= 0) || (sy_0 <= 0 && error_0 > 0)) {
+                    if (swap_0)
+                        x_0 += sx_0;
+                    else {
+                        y_0 += sy_0;
+                        stop_0 = true;
+                    }
+                    error_0 -= 2 * dx_0;
+                }
+
+                if (swap_0) {
+                    y_0 += sy_0;
+                    stop_0 = true;
+                } else
+                    x_0 += sx_0;
+                error_0 += 2 * dy_0;
+                i_0++;
+            }
+
+            if (i_1 <= dx_1 && !stop_1) {
+                point(x_1, y_1, color);
+                while ((sy_1 > 0 && error_1 >= 0) || (sy_1 <= 0 && error_1 > 0)) {
+                    if (swap_1)
+                        x_1 += sx_1;
+                    else {
+                        y_1 += sy_1;
+                        stop_1 = true;
+                    }
+                    error_1 -= 2 * dx_1;
+                }
+                if (swap_1) {
+                    y_1 += sy_1;
+                    stop_1 = true;
+                } else
+                    x_1 += sx_1;
+                error_1 += 2 * dy_1;
+                i_1++;
+            }
+        }
+    }
+
+    public void triangle(int x0, int y0, int x1, int y1, int x2, int y2, int color) {
+        int xMin = Math.min(x0, Math.min(x1, x2));
+        int yMin = Math.min(y0, Math.min(y1, y2));
+        int xMax = Math.max(x0, Math.max(x1, x2));
+        int yMax = Math.max(y0, Math.max(y1, y2));
+
+        for (int y = yMin; y <= yMax; y++) {
+            float xCross0 = xMax;
+            float xCross1 = xMin;
+            float xCross = xCross(xMin, y, xMax, y, x0, y0, x1, y1);
+            int nanCount = 0;
+            if (Float.isNaN(xCross)) nanCount++;
+            if (!Float.isNaN(xCross)) {
+                if (xCross < xCross0) xCross0 = xCross;
+                if (xCross > xCross1) xCross1 = xCross;
+            }
+            xCross = xCross(xMin, y, xMax, y, x1, y1, x2, y2);
+            if (Float.isNaN(xCross)) nanCount++;
+            if (!Float.isNaN(xCross)) {
+                if (xCross < xCross0) xCross0 = xCross;
+                if (xCross > xCross1) xCross1 = xCross;
+            }
+            xCross = xCross(xMin, y, xMax, y, x2, y2, x0, y0);
+            if (Float.isNaN(xCross)) nanCount++;
+            if (!Float.isNaN(xCross)) {
+                if (xCross < xCross0) xCross0 = xCross;
+                if (xCross > xCross1) xCross1 = xCross;
+            }
+            int xCross0int = Math.round(xCross0);
+            int xCross1int = -Math.round(-xCross1);
+            for (int x = xCross0int; x <= xCross1int; x++) {
+                point(x, y, color);
+            }
+        }
+    }
+
+    private float xCross(float x00, float y00, float x01, float y01,
+                         float x10, float y10, float x11, float y11
+    ) {
+        float xMin = Math.min(x10, x11);
+        float yMin = Math.min(y10, y11);
+        float xMax = Math.max(x10, x11);
+        float yMax = Math.max(y10, y11);
+        float a0 = y01 - y00;
+        float b0 = x00 - x01;
+        float c0 = x00 * (y00 - y01) + y00 * (x01 - x00);
+        float a1 = y11 - y10;
+        float b1 = x10 - x11;
+        float c1 = x10 * (y10 - y11) + y10 * (x11 - x10);
+
+        float D = a0 * b1 - a1 * b0;
+        float Dx = -c0 * b1 + c1 * b0;
+        float Dy = -a0 * c1 + a1 * c0;
+
+        if (D != 0) {
+            float x = Dx / D;
+            float y = Dy / D;
+            float e = -0.001f;
+            if (x - xMin > e && xMax - x > e && y - yMin > e && yMax - y > e) {
+                return x;
+            }
+        }
+
+        return Float.NaN;
     }
 
     public void flush(int[] to) {
